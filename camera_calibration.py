d722a6566a7f8fce09fd04845751caee18ea7036
Eyoh-original <ammar.istariq@gmail.com>
2026-07-07 12:33:20 +0100

    Refactor camera calibration with camera switcher

--- camera_calibration.py
+++ camera_calibration.py
@@ -2,121 +2,157 @@ from picamera2 import Picamera2
 import cv2
 import numpy as np
 import time
+import RPi.GPIO as GPIO
+import os
 
-# Chessboard dimensions
-chessboard_size = (7, 7)
+class CameraSwitcher:
 
-# Prepare object points
-objp = np.zeros((chessboard_size[0] * chessboard_size[1], 3),
-                np.float32)
-objp[:, :2] = np.mgrid[
-    0:chessboard_size[0],
-    0:chessboard_size[1]
-].T.reshape(-1, 2)
+    def __init__(self):
 
-objpoints = []
-imgpoints = []
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
 
-# Start Pi Camera
-picam2 = Picamera2()
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
 
-config = picam2.create_preview_configuration(
+    def select(self, camera):
+
+        if camera == "A":
+
+            os.system("i2cset -y 10 0x70 0x00 0x04")
+
+            GPIO.output(7, False)
+            GPIO.output(11, False)
+            GPIO.output(12, True)
+
+        elif camera == "B":
+
+            os.system("i2cset -y 10 0x70 0x00 0x05")
+
+            GPIO.output(7, True)
+            GPIO.output(11, False)
+            GPIO.output(12, True)
+
+        time.sleep(0.5)
+
+switcher = CameraSwitcher()
+
+def calibrate_camera(camera):
+
+  #Chessboard Dimensions
+  chessboard_size = (9,6)
+  
+  #Prepare Object Points
+  objp = np.zeros((chessboard_size[0] * chessboard_size[1], 3),
+                   np.float32)
+  objp[:, :2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1, 2)
+  objpoints = []
+  imgpoints = []
+  
+  switcher.select(camera)
+  
+  picam2 = Picamera2()
+  config = picam2.create_preview_configuration(
     main={"size": (1280, 720)}
-)
-
-picam2.configure(config)
-picam2.start()
-
-# Give camera time to settle
-time.sleep(2)
-
-print("Press SPACE to capture calibration image")
-print("Press Q to finish calibration")
-
-img_count = 0
-
-while True:
-
-    # Capture frame from Pi Camera
+  )
+  
+  picam2.configure(config)
+  picam2.start()
+  
+  time.sleep(2)
+  
+  print("Press SPACE to capture calibration images")
+  print("Press Q to finish calibration")
+  
+  img_count = 0
+  while True:
     frame = picam2.capture_array()
-
-    # Picamera2 returns RGB
+    
     frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
     gray = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
-
+    
     ret_cb, corners = cv2.findChessboardCorners(
-        gray,
-        chessboard_size,
-        None
+      gray,
+      chessboard_size,
+      None, 
     )
-
     display = frame_bgr.copy()
-
+    
     if ret_cb:
-        cv2.drawChessboardCorners(
-            display,
-            chessboard_size,
-            corners,
-            ret_cb
-        )
-
+      cv2.drawChessboardCorners(
+        display, 
+        chessboard_size, 
+        corners, 
+        ret_cb
+      )
+      
     cv2.imshow("Calibration", display)
-
     key = cv2.waitKey(1) & 0xFF
-
+      
     if key == ord(' '):
-
+        
         if ret_cb:
-
-            objpoints.append(objp)
-            imgpoints.append(corners)
-
-            cv2.imwrite(
-                f"calibration_{img_count}.jpg",
-                frame_bgr
-            )
-
-            img_count += 1
-
-            print(f"Captured calibration image {img_count}")
-
+          
+          objpoints.append(objp)
+          imgpoints.append(corners)
+          
+          cv2.imwrite(
+            f"{camera}calibration_{img_count}.jpg",
+            frame_bgr
+          )
+          
+          img_count += 1
+          
+          print(f"Captured calibration image {img_count}")
+        
         else:
-            print("Chessboard not detected")
+          print("Chessboard not detected")
+        
+      elif key == ord('q'):
+          break
+    
+  picam2.stop()
+  cv2.destroyAllWindows()
 
-    elif key == ord('q'):
-        break
-
-picam2.stop()
-cv2.destroyAllWindows()
-
-if len(objpoints) < 5:
+  if len(objpoints) < 15:
     print("Not enough calibration images.")
-    exit()
+    return None, None
+    
+  print("Calculating Calibration")
 
-print("Calculating calibration...")
-
-ret, camera_matrix, dist_coeffs, rvecs, tvecs = \
-    cv2.calibrateCamera(
-        objpoints,
-        imgpoints,
-        gray.shape[::-1],
-        None,
-        None
-    )
-
-np.savez(
-    "camera_calib.npz",
+  ret, camera_matrix, dist_coeffs, rvecs, tvecs = \
+          cv2.calibrateCamera(
+            objpoints, 
+            imgpoints, 
+            gray.shape[::-1],
+            None, 
+            None
+          )
+  np.savez(
+      f"{camera}_calib.npz",
     camera_matrix=camera_matrix,
     dist_coeffs=dist_coeffs
-)
+    )
+  
+  print("Calibration saved to" f"{camera}_calib.npz")
+  print("\nCamera Matrix:")
+  print(camera_matrix)
+  print("\nDistortion Coefficients")
+  print(dist_coeffs)
+  
+  return camera_matrix, dist_coeffs 
+    
+def main():
+  leftMatrix, leftDist = calibrate_camera("A")
 
-print("Calibration saved to camera_calib.npz")
+  rightMatrix, rightDist = calibrate_camera("B")
 
-print("\nCamera Matrix:")
-print(camera_matrix)
-
-print("\nDistortion Coefficients:")
-print(dist_coeffs)
-
-print("RMS:", ret)
+  print(rightMatrix)
+  print(rightDist)
+  print(leftMatrix)
+  print(leftDist)
+  
+if __name__ == "__main__":
+    main()
+  GPIO.cleanup()
