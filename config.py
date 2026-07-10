0cc20ea106161a378a7bde286dd4611b32667ced
ggalaxyv99 <glvpadmavathi@gmail.com>
2026-07-10 21:57:06 +0100

    Revert "Electrical subsystem with telemetry and safetry monitoring"
    This reverts commit 1884f1a5f7cb93a4e127f7a50fd920c9885226aa.

--- config.py
+++
@@ -1,6 +0,0 @@
-# Battery Limits
-MIN_BATTERY_VOLTAGE = 6.4      # Volts
-MAX_BATTERY_CURRENT = 2.0      # Amps
-
-# Nominal values
-FULL_BATTERY_VOLTAGE = 8.4
--- logger.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-def log(telemetry):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("telemetry_log.txt", "a") as log_file:
-        log_file.write(f"{current_time} \n")
-
-        for key, value in telemetry.items():
-            log_file.write(f"{key}: {value}\n") 
